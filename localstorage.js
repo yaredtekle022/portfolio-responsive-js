880f01b9f4fb61d76940eb8abccc82681a32f909
yaredtt <yaredtekle22@gmail.com>
2022-12-23 14:52:24 +0300

    fix conflict

--- localstorage.js
+++ localstorage.js
@@ -1,34 +1,145 @@
-const usernameInput = document.getElementById('fname');
-const emailInput = document.getElementById('email');
-const textareaInput = document.getElementById('textarea');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navi-menu');
+const logo = document.querySelector('.logo');
+const nav = document.querySelector('.nav');
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  logo.classList.toggle('active');
+  nav.classList.toggle('active');
+}
+hamburger.addEventListener('click', mobileMenu);
+const naviLink = document.querySelectorAll('.navi-link');
 
-// check local storage for user
-
-const userInfo = JSON.parse(localStorage.getItem('userInfo'));
-if (userInfo) {
-  usernameInput.value = userInfo.username;
-  emailInput.value = userInfo.email;
-  textareaInput.value = userInfo.textarea;
+function closeMenu() {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+  logo.classList.remove('active');
+  nav.classList.remove('active');
 }
 
-document.getElementById('form').addEventListener('submit', (e) => {
-  e.preventDefault();
+naviLink.forEach((n) => n.addEventListener('click', closeMenu));
 
-  // get values from nput
-  const username = usernameInput.value.trim();
-  const email = emailInput.value.trim();
-  const textarea = textareaInput.value.trim();
+// pop-up
+const projectDatas = [
+  {
+    popupHeadingOne: 'Multi Post Stories',
+    popImg: './images/popupMobile.svg',
+    popImgLive: './images/live.svg',
+    popImgSource: './images/GitHub.svg',
+    popupDescriptionOne: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    popupList: ['html', 'Bootstrap', 'Ruby on rails'],
+    popupLiveLink: 'https://yaredtekle022.github.io/portfolio-responsive-js',
+    popupSouceLink: 'https://github.com/yaredtekle022/portfolio-responsive-js',
+  },
+  {
 
-  if (!username || !email || !textarea) {
-    return;
+    popupHeadingTwo: 'Keeping track of hundreds  of components website',
+    popImg: './images/popupMobile.svg',
+    popImgLive: './images/live.svg',
+    popImgSource: './images/GitHub.svg',
+    popupDescriptionTwo: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+    popupList: ['html', 'Bootstrap', 'Ruby on rails'],
+    popupLiveLink: 'https://yaredtekle022.github.io/portfolio-responsive-js',
+    popupSouceLink: 'https://github.com/yaredtekle022/portfolio-responsive-js',
+  }];
+
+const popup = document.querySelector('.modal');
+
+popup.innerHTML = `<div class="pop-up-header">
+<h2 class="popheading1">${projectDatas[0].popupHeadingOne}</h2>
+<h2 class="popheading2">${projectDatas[1].popupHeadingTwo}</h2>
+<button data-close-button class="close-button">&times;</button>
+</div>
+<ul class="pop-up-lists">
+<a class="pop-list" href="#">
+  <li>${projectDatas[0].popupList[0]}</li>
+</a>
+<a class="pop-list" href="#">
+  <li>${projectDatas[0].popupList[1]}</li>
+</a>
+<a class="pop-list" href="#">
+  <li>${projectDatas[0].popupList[2]}</li>
+</a>
+</ul>
+<div class="popcon">
+<img src="${projectDatas[0].popImg}" alt="popup" />
+<div class="poptextbutton">
+<p class="popuppharagraph1">
+${projectDatas[0].popupDescriptionOne}
+</p>
+<p class="popuppharagraph2">
+${projectDatas[1].popupDescriptionTwo}
+</p>
+<div class="pop-up-btns">
+<a class="pop-btn" href="${projectDatas[0].popupLiveLink}">
+  <p>See Live</p> <img src="${projectDatas[0].popImgLive}" alt="Live" />
+</a>
+<a class="pop-btn" href="${projectDatas[0].popupSouceLink}">
+  <p>See Source</p> <img src="${projectDatas[0].popImgSource}" alt="Github" />
+</a>
+</div>
+</div>
+</div>`;
+
+const popupOverlay = document.querySelector('#overlay');
+
+popupOverlay.innerHTML = "<div id='overlay'></div>";
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
+
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+    window.scrollTo({ top: 10, behavior: 'smooth' });
+  });
+});
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
+
+// Form Validation
+const form = document.querySelector('#form');
+const email = document.getElementById('email');
+const emailError = document.querySelector('.error');
+
+form.addEventListener('submit', (event) => {
+  if (email.value.match(/[A-Z]/g)) {
+    event.preventDefault();
+    emailError.textContent = 'Please use lowercases for the email field';
+  } else if (email.validity.valueMissing) {
+    event.preventDefault();
+    emailError.textContent = 'You need to enter an e-mail address.';
+  } else if (email.validity.typeMismatch) {
+    event.preventDefault();
+    emailError.textContent = 'Entered value needs to be an e-mail address.';
+  } else if (email.validity.tooShort) {
+    event.preventDefault();
+    emailError.textContent = 'You need to enter an e-mail address.';
   }
-
-  // saving inputs
-  const userInfo = {
-    username,
-    email,
-    textarea,
-  };
-
-  localStorage.setItem('userInfo', JSON.stringify(userInfo));
 });
